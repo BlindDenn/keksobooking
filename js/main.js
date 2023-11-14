d1f4cf54c97ff7d4b91ae298ae76820168c3228b
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-11-14 22:20:37 +0300

    feat [offers.js]: featuresList creating

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// import{ getDataArray } from './mock-data.js';
 import{ generateOffers } from './offers.js';
 
-// getDataArray();
 generateOffers();
-
-// console.log(getDataArray());
--- js/offers.js
+++ js/offers.js
@@ -1,21 +1,38 @@
 import { getDataArray } from './mock-data.js';
 
-const offerTemplate = document.querySelector('#card').content.querySelector('.popup');
 const mapCanvas = document.querySelector('#map-canvas');
-
+const offerTemplate = document.querySelector('#card').content.querySelector('.popup');
 const dataArray = getDataArray();
+const FEATURE_CLASS_MOD_SUBSTR = 'popup__feature--';
+const FEATURE_CLASS_MOD_SUBSTR_INDEX = 1;
 
-const generateOffer = ({offer:{title}}) => {
-  const offer = offerTemplate.cloneNode(true);
-  offer.querySelector('.popup__title').textContent = title;
-  // offer.querySelector('.popup__title').textContent = data.offer.title;
-  mapCanvas.append(offer);
+const generateOffersFeatures = (parent, sourceData) => {
+  const featuresList = parent.querySelector('.popup__features');
+  const featureItems = featuresList.querySelectorAll('.popup__feature');
+
+  featureItems.forEach((item) => {
+    const classUnicSubstring = item.classList[FEATURE_CLASS_MOD_SUBSTR_INDEX].slice(FEATURE_CLASS_MOD_SUBSTR.length);
+    const isPresent = sourceData.some((element) => element === classUnicSubstring);
+    if(isPresent) {
+      item.remove();
+    }
+  }
+  );
+};
+
+const generateOffer = ({offer:{title, features}}) => {
+
+  const newOffer = offerTemplate.cloneNode(true);
+
+  newOffer.querySelector('.popup__title').textContent = title;
+  generateOffersFeatures(newOffer, features);
+
+  mapCanvas.append(newOffer);
 };
 
 const generateOffers = () => {
   dataArray.forEach((data) => generateOffer(data));
 };
 
-getDataArray();
 
 export { generateOffers };
